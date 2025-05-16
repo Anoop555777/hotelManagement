56f7f19609f0b2d1bfb996dd08b1633d138dfaa5
Anoop Singh <66582384+Anoop555777@users.noreply.github.com>
2025-05-16 12:25:49 +0530

    Update supabase.js

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,7 +1,5 @@
-import { createClient } from "@supabase/supabase-js";
-
-const supabaseUrl = "https://sfzhqxiipwnwfsoaateg.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNmemhxeGlpcHdud2Zzb2FhdGVnIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODgyNDA0NjcsImV4cCI6MjAwMzgxNjQ2N30.wIj7KY1Aab-x0-m_Nk6VcZOqEijx0MLde6AkRSH2fmk";
-const supabase = createClient(supabaseUrl, supabaseKey);
+import { createClient } from '@supabase/supabase-js'
+const supabaseUrl = 'https://vdetvdkaahmcbklyrdfq.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZkZXR2ZGthYWhtY2JrbHlyZGZxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU2NTI0OTYsImV4cCI6MjA2MTIyODQ5Nn0.n2Cwj1kIYp0drNscsFrHCoEtqpN5bDDyNm3rLR_E90M
+const supabase = createClient(supabaseUrl, supabaseKey)
 export default supabase;
